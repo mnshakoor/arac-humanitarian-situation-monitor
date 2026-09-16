b426decdb7a73ac7894006215b0250ac4598b553
Nuri Shakoor <91979969+mnshakoor@users.noreply.github.com>
2026-09-16 14:39:45 -0400

    chore: bump AHSM beta to v0.5.0

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   appName: 'ARAC Humanitarian Situation Monitor',
-  version: '0.4.0-beta',
+  version: '0.5.0-beta',
   snapshotUrl: './data/snapshot.json',
   defaultWindow: '30d',
   lowBase: {
