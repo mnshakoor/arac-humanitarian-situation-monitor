8a488621809a67312b5450229fc85a5be23b7cfe
Nuri Shakoor <91979969+mnshakoor@users.noreply.github.com>
2026-09-16 17:21:51 -0400

    chore: bump AHSM maintenance build to v0.8.2-beta

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   appName: 'ARAC Humanitarian Situation Monitor',
-  version: '0.8.1-beta',
+  version: '0.8.2-beta',
   snapshotUrl: './data/snapshot.json',
   defaultWindow: '30d',
   lowBase: {
