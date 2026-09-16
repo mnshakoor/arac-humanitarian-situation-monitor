e626566f6e2a757fab430c5eaee5c0717012a5d0
Nuri Shakoor <91979969+mnshakoor@users.noreply.github.com>
2026-09-16 16:11:11 -0400

    chore: cache v0.8 application shell assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ahsm-shell-v04';
-const SHELL=['./','./index.html','./css/app.css','./css/ui-patch.css','./css/v04.css','./js/app.js','./js/ui-patch.js','./js/v03.js','./js/v04.js','./js/config.js','./js/signals.js','./js/storage.js','./js/export.js','./js/charts.js','./manifest.webmanifest'];
+const CACHE='ahsm-shell-v08';
+const SHELL=['./','./index.html','./css/app.css','./css/ui-patch.css','./css/v04.css','./css/v05.css','./css/v06.css','./css/v07.css','./css/v08.css','./js/app.js','./js/ui-patch.js','./js/v03.js','./js/v04.js','./js/v05.js','./js/v06.js','./js/v07.js','./js/v07-style.js','./js/v08.js','./js/v08-style.js','./js/region-map.js','./js/config.js','./js/signals.js','./js/storage.js','./js/export.js','./js/charts.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
