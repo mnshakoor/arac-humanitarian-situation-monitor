ec3a634c875389ef5f6c6629a1cb4ee7bcb76c6f
Nuri Shakoor <91979969+mnshakoor@users.noreply.github.com>
2026-09-17 01:31:46 -0400

    release: advance build to v0.9.0-rc2

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   appName: 'ARAC Humanitarian Situation Monitor',
-  version: '0.9.0-rc1.1',
+  version: '0.9.0-rc2',
   snapshotUrl: './data/snapshot.json',
   defaultWindow: '30d',
   lowBase: {
